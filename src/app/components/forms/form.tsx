ad40a6ddad1790a577df6ebe96889866d00edd40
Eliabr Rodrigues <eliabe859@gmail.com>
2025-08-11 16:04:57 -0300

    feat: created the member cancellation form

--- src/app/components/forms/form.tsx
+++ src/app/components/forms/form.tsx
@@ -1,4 +1,5 @@
 import { MemberRegistrationForm } from "./memberRegistrationForm";
+import { MemberCancellationForm } from "./memberCancellationForm";
 import SelectNameCongregations from "./selectNameCongregations";
 
 export function Form() {
@@ -6,6 +7,7 @@ export function Form() {
     <form className="my-10 grid gap-10 ">
       <SelectNameCongregations />
       <MemberRegistrationForm />
+      <MemberCancellationForm />
     </form>
   );
 }
